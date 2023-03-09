2f941a560b805ba36a065b0d4ae9813bf4c82773
ms.alievaalya@gmail.com <ms.alievaalya@gmail.com>
2023-03-09 14:02:07 +0300

    Add project files.

---
+++ NP_HOMEWORK1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Drawing;
+using System.Diagnostics.Metrics;
+using System.Reflection.Metadata.Ecma335;
+using System.Reflection;
+
+
+//КЛИЕНТ
+namespace NP_HOMEWORK1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Timers.Timer timer = new System.Timers.Timer(1000);
+            timer.Elapsed += Timer_Elapsed;
+            timer.Start();
+            while (timer.Enabled)
+            {
+
+            }
+        }
+            private static void Timer_Elapsed(object? sender, System.Timers.ElapsedEventArgs e)
+            {
+                Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
+                IPEndPoint point = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 80);
+                try
+                {
+                    socket.Connect(point);
+                    byte[] buffer = new byte[1024];
+                    int c;
+                    do
+                    {
+                        c = socket.Receive(buffer);
+                        Console.WriteLine(Encoding.UTF8.GetString(buffer));
+                    }
+                    while (c > 0);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                }
+                finally
+                {
+                    socket.Shutdown(SocketShutdown.Both);
+                    socket.Close();
+                }
+            }
+        
+    }
+}
